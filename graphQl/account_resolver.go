9c06d098cc1bdc57ed73f6e2017506b598143ddd
akash1723tripathi <akashtripathi.acad@gmail.com>
2026-08-23 18:17:30 +0530

    chores : resolver functions for GraphQl

--- graphQl/account_resolver.go
+++ graphQl/account_resolver.go
@@ -1,1 +1,11 @@
-Orders
+package graphql
+
+import "context"
+
+type accountResolver struct {
+	server *Server
+}
+
+func (r *accountResolver) Orders(ctx context.Context, obj *Account) ([]*Order, error) {
+	return obj.Orders, nil
+}
--- graphQl/query_resolver.go
+++ graphQl/query_resolver.go
@@ -1,5 +1,28 @@
-Accounts
+package graphql
 
+import (
+	"context"
+)
 
+type queryResolver struct {
+	server *Server
+}
 
-Products
+func (r *queryResolver) Accounts(ctx context.Context, pagination *PaginationInput, id *string) ([]*Account, error) {
+	return []*Account{}, nil
+}
+func (r *queryResolver) Products(ctx context.Context, pagination *PaginationInput, query *string, id *string) ([]*Product, error) {
+	return []*Product{}, nil
+}
+
+func (p PaginationInput) bounds() (uint64, uint64) {
+	skipValue := uint64(0)
+	takeValue := uint64(100)
+	if p.Skip != nil {
+		skipValue = uint64(*p.Skip)
+	}
+	if p.Take != nil {
+		takeValue = uint64(*p.Take)
+	}
+	return skipValue, takeValue
+}
