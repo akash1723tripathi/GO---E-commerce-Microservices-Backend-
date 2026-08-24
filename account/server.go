7d3895bf4384d4c91e42210286f82e0ca1e247b1
akash1723tripathi <akashtripathi.acad@gmail.com>
2026-08-24 22:53:22 +0530

    chores : server.go for account service

--- account/server.go
+++ account/server.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 
+	"github.com/akash1723tripathi/go-microservices/account/pb/pb"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
@@ -19,37 +20,54 @@ func ListenGRPC(s Service, port int) error {
 		return nil, err
 	}
 	serv := grpc.NewServer()
-	pb.(serv,)   // work on it
+	pb.RegisterAccountServiceServer(serv, &grpcServer{s})    // call register server function
 	reflection.Register(serv)
 	return serv.listen(lis)
 
 }
 
-func (s *grpcServer) PostAccount(ctx context.Context, r *pb.) (*pb., error){   
+func (s *grpcServer) PostAccount(ctx context.Context, r *pb.PostAccountRequest) (*pb.PostAccountResponse , error){   
 	a, err := s.service.PostAccount(ctx, r.Name)
 	if err != nil {
 		return nil, err
 	}
 
-	return &pb.{}
+	return &pb.PostAccountResponse{Account: &pb.Account{
+		Id: a.ID,
+		Name : a.Name,
+	}}, nil
 
 }
 
-func (s *grpcServer) GetAccount(ctx context.Context, r *pb.) (*pb., error){ 
+func (s *grpcServer) GetAccount(ctx context.Context, r *pb.GetAccountRequest) (*pb.GetAccountResponse , error){ 
 	a, err := s.service.GetAccount()(ctx, r.ID)
 	if err != nil {
 		return nil, err
 	}
 
-	return &pd.{}
+	return &pd.GetAccountResponse{
+		Account: &pb.Account{
+			Id: a.ID,
+			Name : a.Name,
+		},
+	}, nil
 }
 
-func (s *grpcServer) GetAccounts(ctx context.Context, r *pb.) (*pb., error){ 
-	a, err := s.service.GetAccounts()(ctx, r.ID)
+func (s *grpcServer) GetAccounts(ctx context.Context, r *pb.GetAccountsequest) (*pb.GetAccountsResponse, error){ 
+	res, err := s.service.GetAccounts()(ctx, r.ID)
 	if err != nil {
 		return nil, err
 	}
 
-	return &pd.{}
+	accounts := []*pb.Account{}
+	for _,p := range res {
+		accounts = append(accounts, 
+			&pb.Account{
+				Id: p.ID,
+				Name : p.Name,
+			},
+		)
+	}
+	return &pd.GetAccountsResponse{Accounts : accounts, nil}
 
 }
