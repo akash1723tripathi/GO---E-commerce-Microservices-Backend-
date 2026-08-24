211221a6fb68faae0de99734a73b42ce8f904211
akash1723tripathi <akashtripathi.acad@gmail.com>
2026-08-24 21:43:20 +0530

    update : corrections in server.go

--- account/server.go
+++ account/server.go
@@ -1,33 +1,53 @@
 package account
 
-type grpcServer struct{
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type grpcServer struct {
 	service Service
 }
 
-func (s *grpcServer) PostAccount(ctx context.Context, r*pb.) (*pb., error){  
+func ListenGRPC(s Service, port int) error {
+	lis, err := net.Listen("tcp",fmt.Sprintf("%d", port))
+	if err != nil{
+		return nil, err
+	}
+	serv := grpc.NewServer()
+	pb.(serv,)   // work on it
+	reflection.Register(serv)
+	return serv.listen(lis)
+
+}
+
+func (s *grpcServer) PostAccount(ctx context.Context, r *pb.) (*pb., error){   
 	a, err := s.service.PostAccount(ctx, r.Name)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 
 	return &pb.{}
 
 }
 
-
-func (s *grpcServer) GetAccount(ctx context.Context, r*pb. ) (*pb., error){
+func (s *grpcServer) GetAccount(ctx context.Context, r *pb.) (*pb., error){ 
 	a, err := s.service.GetAccount()(ctx, r.ID)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 
 	return &pd.{}
 }
 
-func (s *grpcServer) GetAccounts(ctx context.Context, r*pb. ) (*pb., error){
+func (s *grpcServer) GetAccounts(ctx context.Context, r *pb.) (*pb., error){ 
 	a, err := s.service.GetAccounts()(ctx, r.ID)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 
 	return &pd.{}
